e88c6f8ec08bd11c5cadcdb0eb74177fa1a5b5ab
Meme.rip <92902811+Memerip@users.noreply.github.com>
2022-11-29 22:39:32 -0500

    Create keysystem.lua

---
+++ archive/keysystem.lua
@@ -0,0 +1,119 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Ripper Hub Key", HidePremium = true, IntroText = "Ripper Hub Key", SaveConfig = true, ConfigFolder = "RipperHubKey"})
+if game.GameId ~= 9144187696 then
+
+     OrionLib:MakeNotification({
+	Name = "Warning!",
+	Content = "Invalid game", 
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+setclipboard("https://www.roblox.com/games/9144187696")
+end
+OrionLib:MakeNotification({
+	Name = "Your in!",
+	Content = "Ripper Hub Key v1.2", 
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Memerip/Ripper-Hub/main/script/key.lua"))()
+
+function MakeScriptHub()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Memerip/Ripper-Hub/main/script/ripperhub.lua"))()
+end
+
+
+function CorrectKeyNotifications()
+    OrionLib:MakeNotification({
+        Name = "Correct Key",
+        Content = "Welcome to Ripper Hub!",
+        Image = "rbxassetid://4483345998",
+        Time = 2
+    })
+end
+
+function WrongKeyNotifications()
+    OrionLib:MakeNotification({
+        Name = " Wrong Key",
+        Content = "Join my Discord for the Key!",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+function GameCopiedNotifications()
+    OrionLib:MakeNotification({
+        Name = "Link Copied",
+        Content = "Game link has been copied!",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+function GetKeyNotifications()
+    OrionLib:MakeNotification({
+        Name = "GetKey",
+        Content = "Discord Link Copied!",
+        Image = "rbxassetid://4483345998",
+        Time = 5
+    })
+end
+
+local Enter = Window:MakeTab({
+	Name = "Enter Key",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Get = Window:MakeTab({
+	Name = "Get Key",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Games = Window:MakeTab({
+	Name = "Games",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+Enter:AddTextbox({
+	Name = "Enter Key",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+        _G.KeyInput = Value
+	end	  
+})
+
+Enter:AddButton({
+	Name = "Check Key!",
+	Callback = function()
+        if _G.KeyInput == _G.EnterKey then
+			CorrectKeyNotifications()
+			wait(3)
+         loadstring(game:HttpGet("https://raw.githubusercontent.com/Memerip/Ripper-Hub/main/script/ripperhub.lua"))()
+     else
+        WrongKeyNotifications()
+        end
+  	end    
+})
+
+Get:AddLabel("Get Key")
+Get:AddButton({
+	Name = "Copy link!",
+	Callback = function()
+      		setclipboard("https://discord.com/invite/CKQuGPqx8M")
+			  GetKeyNotifications()
+  	end    
+})
+
+Games:AddLabel("Supported Game")
+Games:AddButton({
+	Name = "A Piece",
+	Callback = function()
+setclipboard("https://www.roblox.com/games/9144187696")
+			GameCopiedNotifications()
+  	end 
+})
