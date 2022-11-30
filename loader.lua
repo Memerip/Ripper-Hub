7196f8f1c4d3b63382948dd84fa92ba6c9c6e792
Meme.rip <92902811+Memerip@users.noreply.github.com>
2022-11-29 23:56:20 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,4 +3,7 @@ if game.PlaceId == 9144187696 then -- A Piece
     
 elseif game.PlaceId == 9387219630 then -- Muscle Clicker
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Memerip/Ripper-Hub/main/script/muscleclicker.lua"))()
+    
+else  -- Universal
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Memerip/Ripper-Hub/main/script/universal.lua"))()
 end
